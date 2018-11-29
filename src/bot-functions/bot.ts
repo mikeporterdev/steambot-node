20ca4f5d69bf36c220614d490ae3f6c8feefe211
Michael Porter <m_porter@live.co.uk>
2018-11-29 00:46:06 +0000

    Extracted bot to own class and returned single subscribable for game with prices

---
+++ src/bot-functions/bot.ts
@@ -0,0 +1,47 @@
+import { SteamApi } from '../api/steam-api';
+import { ItadApi } from '../api/itad-api';
+import { CacheableObservable } from '../models/cacheable-observable';
+import * as Fuse from 'fuse.js';
+import { SimpleSteamApp, SteamGame } from '../models/steam-api.models';
+import { Observable } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
+
+export class Bot {
+  private steamApi: SteamApi;
+  private itadApi: ItadApi;
+  private appListCache: CacheableObservable<SimpleSteamApp[]>;
+  constructor(steamKey: string, itadKey: string) {
+    this.steamApi = new SteamApi(steamKey);
+    this.itadApi = new ItadApi(itadKey);
+    this.appListCache = new CacheableObservable(this.steamApi.getAppList());
+  }
+
+  public getGame(name: string): Observable<SteamGame> {
+    return this.appListCache.runObs().pipe(
+      mergeMap(apps => {
+        const fuse = new Fuse(apps, {
+          shouldSort: true,
+          threshold: 0.6,
+          location: 0,
+          distance: 100,
+          maxPatternLength: 32,
+          minMatchCharLength: 1,
+          keys: ['name'],
+        });
+
+        const steamApps = fuse.search(name);
+        const steamApp = steamApps[0];
+        return this.steamApi.getFullSteamDetails(steamApp).pipe(
+          mergeMap((fullApp: SteamGame) => {
+            return this.itadApi.getPricingInfoForAppId(fullApp.steamAppId).pipe(
+              map(prices => {
+                fullApp.prices = prices;
+                return fullApp;
+              })
+            );
+          })
+        );
+      })
+    );
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,15 @@
 import app from './App';
-import { SteamApi } from './api/steam-api';
-import { CacheableObservable } from './models/cacheable-observable';
-import * as Fuse from 'fuse.js';
-import { RxHR } from '@akanass/rx-http-request';
-import { SteamGame } from './models/steam-api.models';
-import { map } from 'rxjs/operators';
-import { Plain, PlainRequest, Price, Shop } from './models/itad-api.models';
-import { ItadApi } from './api/itad-api';
+import { Bot } from './bot-functions/bot';
 
 const port = process.env.PORT || 3000;
-const steamKey = process.env.STEAM_API_KEY;
-const itadKey = process.env.ITAD_API_KEY;
+const steamKey = process.env.STEAM_API_KEY || '';
+const itadKey = process.env.ITAD_API_KEY || '';
 
-const steamApi = new SteamApi(steamKey);
-const itadApi = new ItadApi(itadKey);
+const bot = new Bot(steamKey, itadKey);
 
-const appListCache = new CacheableObservable(steamApi.getAppList());
-
-appListCache.runObs().subscribe(
-  apps => {
-    const fuse = new Fuse(apps, {
-      shouldSort: true,
-      threshold: 0.6,
-      location: 0,
-      distance: 100,
-      maxPatternLength: 32,
-      minMatchCharLength: 1,
-      keys: ['name'],
-    });
-
-    const steamApps = fuse.search('ark survival evolved');
-    const steamApp = steamApps[0];
-    steamApi.getFullSteamDetails(steamApp).subscribe((fullApp: SteamGame) => {
-      itadApi.getPricingInfoForAppId(fullApp.steamAppId).subscribe(prices => {
-        fullApp.prices = prices;
-        console.log(fullApp.cheapestPrice());
-      });
-    });
-  },
-  err => {
-    console.error('ERR', err);
-  }
-);
+bot.getGame('no mans sky').subscribe(res => {
+  console.log(res);
+});
 
 app.listen(port, (err: Error) => {
   if (err) {
