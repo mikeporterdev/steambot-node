c64a1b2ab6fc4b525a86f59c866ee0d95ef6e56f
Michael Porter <m_porter@live.co.uk>
2018-11-29 01:21:18 +0000

    Added basic Discord functionality and removed test Express code.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,41 @@
-import app from './App';
 import { Bot } from './bot-functions/bot';
+import { Client, RichEmbed } from 'discord.js';
 
 const port = process.env.PORT || 3000;
 const steamKey = process.env.STEAM_API_KEY || '';
 const itadKey = process.env.ITAD_API_KEY || '';
+const discordKey = process.env.DISCORD_API_KEY;
 
 const bot = new Bot(steamKey, itadKey);
+const client = new Client();
 
-bot.getGame('no mans sky').subscribe(res => {
-  console.log(res);
+client.on('ready', () => {
+  console.log(`logged in as ${client.user.tag}`);
 });
 
-app.listen(port, (err: Error) => {
-  if (err) {
-    return console.log(err);
+client.on('message', msg => {
+  const command = 'steambot search ';
+  if (msg.content.startsWith(command)) {
+    const searchString = msg.content.substring(command.length);
+
+    msg.reply(`Searching ${searchString}`);
+
+    bot.getGame(searchString).subscribe(res => {
+      const richEmbed = new RichEmbed();
+      richEmbed.setTitle(res.name);
+      richEmbed.setThumbnail(res.headerImageUrl);
+      richEmbed.setDescription(res.shortDescription);
+      richEmbed.setAuthor('SteamBot');
+      const cheapest = res.cheapestPrice();
+      richEmbed.addField('Price', `£${cheapest.priceNew} ([${cheapest.shop.name}](${cheapest.url}))`);
+      richEmbed.addField('Metacritic', `${res.metacritic.score}%`);
+
+      msg.reply(richEmbed);
+    }, err => {
+      console.log(err);
+      msg.reply(`Whoops, error occurred searching ${searchString}`);
+    })
   }
-
-  return console.log(`server is listening on ${port}`);
 });
+
+client.login(discordKey);
