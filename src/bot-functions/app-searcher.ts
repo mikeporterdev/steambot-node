c14f601c1759abc3cc3af33d2638dfbdb289c7ab
Michael Porter <m_porter@live.co.uk>
2018-11-30 18:38:15 +0000

    Tidied app searcher and extracted fuzzy searcher creation to it's own method

--- src/bot-functions/app-searcher.ts
+++ src/bot-functions/app-searcher.ts
@@ -5,45 +5,18 @@ import { Sortable } from '../functions/sortable';
 
 export class AppSearcher {
   public method(apps: SimpleSteamApp[], name: string, filteredIds: number[]) {
-    const wordsToFilter = ['Trailer', 'DLC', 'Teaser', 'Demo'];
+    const fuse = this.getFuzzySearcher(apps, name);
 
-    apps = apps
-      .filter(
-        i =>
-          !wordsToFilter.some(
-            word =>
-              i.name.toLowerCase().includes(word.toLowerCase()) && !name.toLowerCase().includes(word.toLowerCase())
-          )
-      )
-      .map(i => {
-        i.name = i.name.replace(/[^0-9a-z ]/gi, '');
-        return i;
-      });
-
-    const fuse = new Fuse(apps, {
-      shouldSort: true,
-      threshold: 0.4,
-      location: 0,
-      distance: 100,
-      maxPatternLength: 32,
-      minMatchCharLength: 1,
-      keys: ['name'],
-      includeScore: true,
-    });
-
-    name = name.replace(/[^0-9a-z ]/gi, '');
-
+    name = this.strip(name);
     if (name.length === 0) {
       return throwError(new Error('Input is blankspace'));
     }
+
     // cast fuse results because their typings don't include the obj structure change when includeScore is set to true
     // @ts-ignore
     const sortedByClosest = fuse.search(name) as Array<{ item: SimpleSteamApp; score: number }>;
-
     const sortByField = new Sortable(sortedByClosest).sortByField('score');
-
     const closestMatching = sortByField.filter(app => app.score < 0.4);
-
     const closestMatchingWithoutPreviouslyFailedIds = closestMatching.filter(
       i => !filteredIds.some(j => j === i.item.appId)
     );
@@ -53,5 +26,37 @@ export class AppSearcher {
     }
 
     return closestMatchingWithoutPreviouslyFailedIds.map(i => i.item);
+  }
+
+  private getFuzzySearcher(apps: SimpleSteamApp[], name: string) {
+    const wordsToFilter = ['Trailer', 'DLC', 'Teaser', 'Demo'];
+
+    apps = apps
+      .filter(
+        i =>
+          !wordsToFilter.some(
+            word =>
+              i.name.toLowerCase().includes(word.toLowerCase()) && !name.toLowerCase().includes(word.toLowerCase()),
+          ),
+      )
+      .map(i => {
+        i.name = this.strip(i.name);
+        return i;
+      });
+
+    return new Fuse(apps, {
+      shouldSort: true,
+      threshold: 0.4,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: ['name'],
+      includeScore: true,
+    });
+  }
+
+  private strip(str: string) {
+    return str.replace(/[^0-9a-z ]/gi, '');
   }
 }
